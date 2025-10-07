3e39d6441ff87ae73f9a2384bfa697ab0b606c8e
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2025-10-07 12:32:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from config import SECRET_KEY, DEBUG, HOST, PORT
+from database import init_users_db
+from routes import register_blueprints
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+# Регистрация blueprints
+register_blueprints(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    init_users_db()
+    app.run(debug=DEBUG, host=HOST, port=PORT, threaded=True)
